daa022b723694644a42df7d2f1ef31fef6c5262f
joey <13169840505@163.com>
2018-03-01 11:28:09 +0800

    添加得到验证码函数

--- js/common.js
+++ js/common.js
@@ -7,3 +7,12 @@ function randomRange(min,max){
 
     return parseInt(Math.random() * (max - min + 1) + min);
 }
+
+function verification(){
+    var code='';
+    var codeStr='0123456789abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ';
+    for (let i = 0; i < 4; i++) {
+        code += codeStr[parseInt(Math.random()*codeStr.length)];
+    }
+    return code;
+}
